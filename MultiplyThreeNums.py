9b4ff3c4bee0fd0aa2759ebf52efac5131f24fba
yyr123456789 <418955330@qq.com>
2025-03-21 15:49:38 +0800

    Add MultiplyThreeNums.py to repository

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,16 @@
+#Author:fanchenxuan
+#Date:21/3/25
+
+#enter three number
+a=float(input("First nummber: "))
+b=float(input("Second nummber: "))
+c=float(input("Third nummber: "))
+
+#Multiply Three Numbers
+res=float(a*b*c)
+
+#output the result
+print(res)
+
+
+
